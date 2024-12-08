bbb95273a2bbf920c35898ae4adaa314036f8093
Sihun23 <gcsw16@Gachonui-MacBookPro-2.local>
2024-12-08 18:19:12 +0900

    :hammer:feat:DTO수정

--- src/main/java/pednav/backend/pednav/dto/AudioMetadataDTO.java
+++ src/main/java/pednav/backend/pednav/dto/AudioMetadataDTO.java
@@ -1,11 +1,10 @@
 package pednav.backend.pednav.dto;
 
 public class AudioMetadataDTO {
-    private String fileName; // 파일 이름
-    private int sampleRate;  // 샘플링 레이트 (예: 44100)
-    private int channels;    // 채널 수 (모노 = 1, 스테레오 = 2)
-    private String format;   // 오디오 형식 (예: PCM)
+    private String fileName;    // 파일 이름
+    private int sampleRate;     // 샘플링 레이트 (예: 44100)
+    private int channels;       // 채널 수 (모노 = 1, 스테레오 = 2)
+    private String format;      // 오디오 형식 (예: PCM)
 
-    // Getter, Setter
-}
+    }
 
