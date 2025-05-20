73fdfb6152a69f15188b8c3a8dd83433bf3df576
Sihun23 <gcsw16@s-MackBook-Pro-16.local>
2025-05-21 04:08:30 +0900

    :wrench:fix:S3연동 수정(IAM 정책 자동연결)

--- src/main/java/pednav/backend/pednav/util/S3Uploader.java
+++ src/main/java/pednav/backend/pednav/util/S3Uploader.java
@@ -3,15 +3,11 @@ package pednav.backend.pednav.util;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
-import java.io.ByteArrayInputStream;
-import java.net.URL;
-import java.nio.file.Paths;
 import java.util.UUID;
 
 @Component
@@ -24,21 +20,12 @@ public class S3Uploader {
     @Value("${aws.s3.region}")
     private String region;
 
-    @Value("${aws.s3.access-key}")
-    private String accessKey;
-
-    @Value("${aws.s3.secret-key}")
-    private String secretKey;
-
     public String uploadPcm(byte[] pcmData, String folder) {
         String fileName = folder + "/" + UUID.randomUUID() + ".pcm";
 
         S3Client s3 = S3Client.builder()
                 .region(Region.of(region))
-                .credentialsProvider(
-                        StaticCredentialsProvider.create(
-                                AwsBasicCredentials.create(accessKey, secretKey)
-                        ))
+                .credentialsProvider(DefaultCredentialsProvider.create()) // 🔄 IAM 역할 기반
                 .build();
 
         s3.putObject(
